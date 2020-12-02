ae060c8a8a278918250ae62cbb1c26d216a1afe5
Daria Muresan <muresan.daria14@yahoo.com>
2020-12-02 23:16:17 +0200

    student-courses component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HeaderComponent } from './header/header.component';
+import { StudentCoursesComponent } from './student-courses/student-courses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StudentCoursesComponent
   ],
   imports: [
     BrowserModule,
