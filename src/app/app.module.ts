eed57201347aba8769bfe999f351bf2e6bef9f71
Daria Muresan <muresan.daria14@yahoo.com>
2020-12-02 23:26:33 +0200

    'student-course-list' and 'student-course-item' components created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HeaderComponent } from './header/header.component';
 import { StudentCoursesComponent } from './student-courses/student-courses.component';
+import { StudentCourseListComponent } from './student-courses/student-course-list/student-course-list.component';
+import { StudentCourseItemComponent } from './student-courses/student-course-list/student-course-item/student-course-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    StudentCoursesComponent
+    StudentCoursesComponent,
+    StudentCourseListComponent,
+    StudentCourseItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/student-courses/student-course-list/student-course-item/student-course-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-course-item',
+  templateUrl: './student-course-item.component.html',
+  styleUrls: ['./student-course-item.component.css']
+})
+export class StudentCourseItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
