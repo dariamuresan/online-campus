d07c220fca42bd71d1071ab8c530d5f90e6bf7e2
Daria Muresan <muresan.daria14@yahoo.com>
2020-12-28 17:46:44 +0200

    Added Navigation with Event Binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'online-campus';
+
+  loadedUser = 'student';
+
+  onNavigation(user : string) {
+    this.loadedUser = user;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component( {
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Output() userSelected = new EventEmitter<string>();
 
+    onSelect(user : string) {
+        this.userSelected.emit(user);
+    }
 }
