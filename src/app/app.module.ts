1e6ed87fc44cd94a1ce955698852da9df2428cfc
Daria Muresan <muresan.daria14@yahoo.com>
2021-01-06 14:25:20 +0200

    Changed the style of some buttons in teacher component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,28 +30,6 @@ import { AdminNewCourseComponent } from './admin-courses/admin-new-course/admin-
 import { LoginComponent } from './authentication/login/login.component';
 import { HelloUserComponent } from './hello-user/hello-user.component';
 
-const appRoutes: Routes = [
-  {path: '', component: HomeComponent, pathMatch: 'full'},
-  {path: 'login', component: LoginComponent},
-  {path: 'student-courses', component: StudentCoursesComponent, children: [
-    {path: '', component: CourseNotSelectedComponent},
-    {path: ':id', component: StudentCourseDetailComponent}
-  ]},
-  {path: 'teacher-courses', component: TeacherCoursesComponent, children: [
-    {path: '', component: TeacherCourseListComponent},
-    {path: ':id', component: TeacherCourseDetailComponent},
-    {path: ':id/edit', component: TeacherCoursesEditComponent},
-    {path: ':id/new', component: AddStudentToCourseComponent}
-  ]},
-  {path: 'admin-courses', component: AdminCoursesComponent, children: [
-    {path: '', component: CourseNotSelectedComponent},
-    {path: 'new', component: AdminNewCourseComponent},
-    {path: ':id', component: AdminCourseEditComponent}
-  ]},
-  {path: 'not-found', component: PageNotFoundComponent},
-  {path: '**', redirectTo: 'not-found'}
-];
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -82,8 +60,7 @@ const appRoutes: Routes = [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes)
+    FormsModule
   ],
   providers: [
     StudentCoursesService, 
